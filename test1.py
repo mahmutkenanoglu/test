29d021873f32dd723841484ed13cbaa08fefecb2
Mahmut Kenanson <mkenanoglu@hotmail.com>
2023-10-31 08:18:04 +0000

    added

---
+++ test1.py
@@ -0,0 +1,23 @@
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(0, 0) == 0
+    assert add(-1, 1) == 0
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(0, 5) == 0
+    assert multiply(-2, 4) == -8
+
+def run_tests():
+    test_add()
+    test_multiply()
+    print("All tests passed!")
+
+if __name__ == "__main__":
+    run_tests()
